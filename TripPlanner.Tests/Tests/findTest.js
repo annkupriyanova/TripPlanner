a3c25705ed035b43df8a0f075fe05dc14559384d
annku <annku@DESKTOP-BULUB4B>
2016-06-16 11:35:40 +0300

    js tests

---
+++ TripPlanner.Tests/Tests/findTest.js
@@ -0,0 +1,61 @@
+﻿/// <reference path="jasmine.js" />
+
+describe("initMap", function () {
+    it("initializes a map", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("findPlaces", function () {
+    it("finds nearby places", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("createMarker", function () {
+    it("shows marker on a map", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("getDetails", function () {
+    it("gets place details", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("createRoute", function () {
+    it("send request to make route", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("showRoute", function () {
+    it("draws route on a map", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("sendTrip", function () {
+    it("sends Trip model to controller", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("sendCoordinates", function () {
+    it("sends coordinates to divide places into groups", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("getAddressName", function () {
+    it("changes welcome header", function () {
+        expect(true).toBe(true);
+    });
+});
+
+describe("getModel", function () {
+    it("gets Trip model", function () {
+        expect(true).toBe(true);
+    });
+});
